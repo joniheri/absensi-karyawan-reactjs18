90745d95568ca09b3773e542b19e9d19e085a87c
jonheri-bootcamp <jonheri.bootcamp@gmail.com>
2023-09-08 18:27:31 +0700

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,80 +5,109 @@ export default function Navbar() {
   const location = useLocation();
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary shadow">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          Absensi
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link
-                className={`nav-link ${location.pathname === "/" && "active"}`}
-                aria-current="page"
-                to="/"
-              >
-                Home
-              </Link>
-            </li>
-          </ul>
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link
-                className={`nav-link ${
-                  location.pathname === "/login" && "active"
-                }`}
-                aria-current="page"
-                to="/login"
-              >
-                Login
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Hello, Jon
-              </a>
-              <ul className="dropdown-menu dropdown-menu-end">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
+    <>
+      <div style={{ marginTop: "60px" }}></div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary shadow fixed-bottom border-top">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/" && "active"
+                  }`}
+                  aria-current="page"
+                  to="/"
+                >
+                  <img
+                    src="assets/img/icons/icons8-home-144-black.png"
+                    alt=""
+                    style={{
+                      height: "30px",
+                      width: "auto",
+                      marginRight: "5px",
+                    }}
+                  />
+                  Home
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/login" && "active"
+                  }`}
+                  aria-current="page"
+                  to="/login"
+                >
+                  <img
+                    src="assets/img/icons/icons8-login-96-black.png"
+                    alt=""
+                    style={{
+                      height: "30px",
+                      width: "auto",
+                      marginRight: "5px",
+                    }}
+                  />
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item dropup">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img
+                    src="assets/img/icons/icons8-user-96-black.png"
+                    alt=""
+                    style={{
+                      height: "30px",
+                      width: "auto",
+                      marginRight: "5px",
+                    }}
+                  />
+                  Hello, Jon
+                </a>
+                <ul className="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
